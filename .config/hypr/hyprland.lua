e5588db6ed1795732d0ea21d25444d972ca26971
czM1K3 <misamadera1@gmail.com>
2026-05-16 17:58:18 +0200

    Changed workspace animation direction

--- .config/hypr/hyprland.lua
+++ .config/hypr/hyprland.lua
@@ -176,7 +176,7 @@ hl.animation({ leaf = "windows", enabled = true, speed = 7, bezier = "myBezier" 
 hl.animation({ leaf = "windowsOut", enabled = true, speed = 7, bezier = "default", style = "popin 80%" })
 hl.animation({ leaf = "border", enabled = true, speed = 10, bezier = "default" })
 hl.animation({ leaf = "fade", enabled = true, speed = 7, bezier = "default" })
-hl.animation({ leaf = "workspaces", enabled = true, speed = 3, bezier = "default" })
+hl.animation({ leaf = "workspaces", enabled = true, speed = 3, bezier = "default", style = "slidevert" })
 hl.animation({ leaf = "specialWorkspace", enabled = true, speed = 6, bezier = "default", style = "slidevert" })
 
 hl.device({
